72b97f4e4427163f2958c8fac7c21e9350cb9c32
Michaela Sizemore <michaela.sizemore@gmail.com>
2021-09-14 13:30:33 -0600

    Add code to make gif, gif file, and update homepage notebook

--- page_art.py
+++ page_art.py
@@ -1,69 +1,46 @@
 ###############################################################################
 # Import packages
 
-from netCDF4 import Dataset
+import xarray as xr
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+import matplotlib.animation as animation
 import cartopy.crs as ccrs
-
-from wrf import (getvar, latlon_coords)
+import cartopy.feature as cfeature
 
 ###############################################################################
 # Read in the data, file path will need to be updated
-wrfin = Dataset("/Users/misi1684/pythia-foundations/images/wrfout_d01_2005-08-28_00_00_00")
 
-# Extract sea level pressure and 2m dewpoint temp
-slp = getvar(wrfin, 'slp')
-td2 = getvar(wrfin, 'td2', units='degF')
+ds = xr.open_dataset("daymet_v3_prcp_2013_na_subset.nc", engine="netcdf4")
+prcp = ds.prcp
 
-###############################################################################
-# Plot the data
+# Create figure for plotting, adding land and state features
+fig = plt.figure(figsize=(15, 15), frameon=False)
+proj = ccrs.PlateCarree()
+ax = plt.axes(projection=proj)
+ax.axis('off')
+ax.coastlines()
+ax.add_feature(cfeature.LAND)
+ax.add_feature(cfeature.STATES)
 
-# Get the latitude and longitude coordinate.
-lats, lons = latlon_coords(slp)
+# Create function to loop over precip data and plot each day
+anim_artists = [] # Generate empty list to place plots in
 
-# Generate figure (set its size (width, height) in inches)
-fig = plt.figure(figsize=(10, 8))
+for prcp_slice in prcp:
+    cntr = ax.contourf(prcp_slice.lon,
+                       prcp_slice.lat,
+                       prcp_slice,
+                       transform=proj,
+                       levels=np.arange(0,55,2))
+    anim_artists.append(cntr.collections)
 
-# # Generate axes using Cartopy
-ax = plt.axes(projection=ccrs.Mercator(central_longitude=-75))
-ax.set_extent([-125, -60, 0, 43],)
-
-
-# ax.set_global()
-# ax.coastlines('50m', linewidth=0.8,zorder=11, edgecolor='black')
-
-# Manually set contour levels
-slp_levels = np.arange(980, 1013, 3)
-td2_levels = np.arange(70, 85, 0.5)
-
-# Add filled contours
-# Add the 850 hPa geopotential height contour lines
-levels = np.arange(0, 12., 1)
-contours = plt.contour(lons,
-                       lats,
-                       slp,
-                       levels=slp_levels,
-                       colors="white",
-                       zorder=11,
-                       transform=ccrs.PlateCarree())
-
-plt.clabel(contours, inline=1, fontsize=10, fmt="%i")
-
-# Add the wind speed filled contours
-levels = [52, 54, 60, 64, 70, 76, 80, 88]
-
-wspd_contours = plt.contourf(lons,
-                             lats,
-                             td2,
-                             levels=td2_levels,
-                             cmap="magma",
-                             zorder=10,
-                             transform=ccrs.PlateCarree())
-###############################################################################
-# Plot image
-img = mpimg.imread('/Users/misi1684/pythia-foundations/images/pretty-earth.png')
-ax.imshow(img, transform=ccrs.PlateCarree(central_longitude=-75))
 plt.tight_layout()
-plt.show()
+
+# Call Matplotlib animation function to create gif
+anim = animation.ArtistAnimation(fig, anim_artists, interval=500)
+
+# Uncomment to save gif
+# anim.save('boulder_prcp.gif', writer="pillow")
+
+
+
